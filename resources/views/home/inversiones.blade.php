18e77749c9beb2a6ba7faf4831f79c5e86f8ce38
Vylius Leonavicius <vylius.leonavicius@gmail.com>
2019-10-05 15:59:17 -0300

    Correcciones de la ultima clase.

--- resources/views/home/inversiones.blade.php
+++ resources/views/home/inversiones.blade.php
@@ -1,74 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app', [
+    'pageTitle' => 'Inversiones',
+    'pageSlogan' => 'Duplicamos tus ahorros en el mercado financiero.',
+    'alignmentJumbotron' => 'text-left'
+    ])
+@section('content')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Inversiones</title>
+    <table class="table">
+        <thead>
+            <tr>
 
-</head>
+                <th scope="col">Empresa</th>
+                <th scope="col">Acciones</th>
+                <th scope="col">Valor de Acción</th>
+                <th scope="col">CompraVenta de Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Arcos Dorados</td>
+                <td>100</td>
+                <td>100</td>
+                <td class="row">
+                    <p class="btn btn-primary">Comprar</p>
+                    <p class="btn btn-success ml-3">Vender</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Potato Banking</a> <!-- aqui puede ir un logo, en el href -->
-        <!-- Es el boton hamburguesa abajo. -->
-        <!--  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button> -->
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="{{url('home/index')}}">Home</a>
-                <a class="nav-item nav-link" href="{{url('home/balance')}}">Balance</a>
-                <a class="nav-item nav-link" href="{{url('home/pagoservicios')}}">Pago de Servicios</a>
-                <a class="nav-item nav-link" href="{{url('home/inversiones')}}">Inversiones <span class="sr-only">(current)</span> </a>
-            </div>
-        </div>
-    </nav>
-
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Inversiones</h1>
-            <p class="lead">Duplicamos tus ahorros en el mercado financiero.</p>
-        </div>
-    </div>
-
-
-    <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-
-                    <th scope="col">Empresa</th>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">Valor de Acción</th>
-                    <th scope="col">CompraVenta de Acción</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Arcos Dorados</td>
-                    <td>100</td>
-                    <td>100</td>
-                    <td class="row">
-                        <p class="btn btn-primary">Comprar</p>
-                        <p class="btn btn-success ml-3">Vender</p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <footer class="text-secondary bg-dark fixed-bottom">
-        <div class="container text-center">
-            <!-- <p class="float-right">
-                <a class="text-info" href="#">Back to top</a>
-            </p> -->
-            <p>© PotatoTheft Tech., please don't Google us!</p>
-        </div>
-    </footer>
-</body>
-
-</html>
+@endsection
